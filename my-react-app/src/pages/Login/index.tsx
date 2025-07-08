03f366a00b727308d3cb50e85bcae034ca9b92a9
tanhh <nhoklilom2.0@gmail.com>
2025-07-08 23:50:41 +0700

    add ui login

--- my-react-app/src/pages/Login/index.tsx
+++ my-react-app/src/pages/Login/index.tsx
@@ -1,3 +1,17 @@
-export default function Login() {
-  return <h1>Login</h1>;
+import { LoginForm } from "@/components/login-form";
+
+export default function LoginPage() {
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <div className="text-primary-foreground flex size-6 items-center justify-center rounded-md">
+            <img src="/clb.png" alt="BNA" />
+          </div>
+          Câu lạc bộ Sách và Hành động
+        </a>
+        <LoginForm />
+      </div>
+    </div>
+  );
 }
